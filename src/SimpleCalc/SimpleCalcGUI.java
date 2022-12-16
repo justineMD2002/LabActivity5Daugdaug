df079c9e4998e7d9a7d404a2074007df7aaff374
justineMD2002 <daugdaug.justine.m@gmal.com>
2022-12-16 14:18:28 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -11,12 +11,21 @@ public class SimpleCalcGUI extends JFrame{
     private JLabel lblResult;
     private JTextField tfNumber2;
     private JPanel pnlMain2;
+    SimpleCalcGUI() {
+        btnCompute.addActionListener(e -> {
+            if(cbOperations.getSelectedItem() == "+") tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) + Double.parseDouble(tfNumber2.getText())));
+            else if(cbOperations.getSelectedItem() == "-") tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) - Double.parseDouble(tfNumber2.getText())));
+            else if(cbOperations.getSelectedItem() == "*") tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) * Double.parseDouble(tfNumber2.getText())));
+            else if(cbOperations.getSelectedItem() == "/") tfResult.setText(String.valueOf(Double.parseDouble(tfNumber1.getText()) / Double.parseDouble(tfNumber2.getText())));
+        });
+    }
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.pnlMain);
-        app.setSize(550, 300);
+        app.setSize(600, 300);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setTitle("Simple Calculator");
         app.setVisible(true);
+        app.pnlMain.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
     }
 }
